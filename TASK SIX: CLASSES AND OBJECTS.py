80512dc343998d8380fd42c69c78925926b26453
Rahul Singh <1207770+rsingh2020@users.noreply.github.com>
2020-04-14 23:57:32 -0700

    Update TASK SIX: CLASSES AND OBJECTS.py

--- TASK SIX: CLASSES AND OBJECTS.py
+++ TASK SIX: CLASSES AND OBJECTS.py
@@ -191,6 +191,44 @@ z.displayTime()
 z.displayMinute()
 
 """
+6.Write a Person class with an instance variable, , and a constructor that takes an integer, , as a parameter.
+# The constructor must assign  to  after confirming the argument passed as  is not negative; if a negative argument is passed as ,
+#  the constructor should set  to  and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+#yearPasses() should increase the  instance variable by .
+#amIOld() should perform the following conditional actions:
+#If , print You are young..
+#If  and , print You are a teenager..
+#Otherwise, print You are old..
+"""
+class Person:
+    def __init__(self,age):
+        self.age=age
+
+        if self.age<0:
+            print("Age is not valid , setting age to 0")
+
+            self.age=0
+
+    def yearPasses(self):
+        self.age += 1
+
+    def amIOld(self):
+
+        if (self.age<10):
+            print("You are young")
+
+        elif(10<self.age<20):
+            print("You are a teenager")
+        else:
+            print("You are old")
+
+
+
+sf=Person(15)
+
+sf.amIOld()
+
+
 
 
 
